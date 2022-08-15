49d1618328f821300e1a82c52474b795b4410c93
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-15 09:39:04 -0300

    aula 135 fix: function to format milliseconds into minutes and seconds

--- src/util/Format.js
+++ src/util/Format.js
@@ -7,4 +7,16 @@ class Format {
         //irá percorrer pelo documento e vai me trazer um array com todas as chaves que ele vai encontrar dentro de um objeto selecionado
         return Object.keys(div.firstChild.dataset)[0];
     }
+
+    static toTime(duration){
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    
+        if (hours > 0) {
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        } else {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`
+        }
+    }
 }
