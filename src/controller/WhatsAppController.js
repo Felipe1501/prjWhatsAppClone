c64aa65f1218eef6ee900f79bc661c177813e36e
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-17 12:12:15 -0300

    aula 161 fix: sending a text message.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -5,6 +5,8 @@ import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from '../util/Firebase';
 import { User } from '../model/User';
 import { Chat } from '../model/Chat';
+import { Message } from '../model/Message';
+import { Message } from '../model/Message';
 
 export class WhatsAppController{
     constructor(){
@@ -130,21 +132,8 @@ export class WhatsAppController{
 
             div.on('click', e=>{
 
-                console.log('cahtId', contact.chatId);
-
-                this.el.activeName.innerHTML = contact.name;
-                this.el.activeStatus.innerHTML = contact.status;
-
-                if(contact.photo){
-                   let img = this.el.activePhoto;
-                   img.src = contact.photo;
-                   img.show();
-                }
-
-                this.el.home.hide();
-                this.el.main.css({
-                    display: 'flex'
-                });
+                this.setActiveChat(contact);
+              
             });
 
             this.el.contactsMessagesList.appendChild(div);
@@ -152,6 +141,25 @@ export class WhatsAppController{
         });
 
         this._user.getContacts();
+    }
+
+    setActiveChat(contact){
+
+        this._contactActive = contact;
+
+        this.el.activeName.innerHTML = contact.name;
+        this.el.activeStatus.innerHTML = contact.status;
+
+        if(contact.photo){
+           let img = this.el.activePhoto;
+           img.src = contact.photo;
+           img.show();
+        }
+
+        this.el.home.hide();
+        this.el.main.css({
+            display: 'flex'
+        });
     }
 
     //vai percorrer pelo documento e vai pegar todos os IDs
@@ -524,7 +532,16 @@ export class WhatsAppController{
         });
 
         this.el.btnSend.on('click', e =>{
-            console.log(this.el.inputText.innerHTML);
+            
+
+            Message.send(
+                this._contactActive.chatId,
+                this._user.email,
+                'text', 
+                this.el.inputText.innerHTML);
+
+            this.el.inputText.innerHTML = '';
+            this.el.panelEmojis.removeClass('open');
         });
 
         this.el.btnEmojis.on('click', e=>{
