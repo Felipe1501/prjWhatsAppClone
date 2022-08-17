daa133c97cb379c594a0042bd1bfb3f26566d539
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-17 11:57:36 -0300

    aula 160 add: Class Chat - Creating a conversation and using btoa() base64 conversion function

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ import {MicrophoneController} from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from '../util/Firebase';
 import { User } from '../model/User';
+import { Chat } from '../model/Chat';
 
 export class WhatsAppController{
     constructor(){
@@ -128,6 +129,9 @@ export class WhatsAppController{
             }
 
             div.on('click', e=>{
+
+                console.log('cahtId', contact.chatId);
+
                 this.el.activeName.innerHTML = contact.name;
                 this.el.activeStatus.innerHTML = contact.status;
 
@@ -295,11 +299,23 @@ export class WhatsAppController{
 
             contact.on('datachange', data=>{
                 if (data.name){
-                    this._user.addContact(contact).then(()=>{
 
-                        this.el.btnClosePanelAddContact.click();
-                        console.info('Contato foi adicionado!');
+                    Chat.createIfNotExists(this._user.email, contact.email).then(chat => {
+                        
+                        contact.chatId = chat.id;
+
+                        this._user.chatId = chat.id;
+
+                        contact.addContact(this._user);
+                        
+                        this._user.addContact(contact).then(()=>{
+
+                            this.el.btnClosePanelAddContact.click();
+                            console.info('Contato foi adicionado!');
+                        });
                     });
+
+                    
                 }else {
                     console.error('Usuário não foi encontrado.');
                 }
