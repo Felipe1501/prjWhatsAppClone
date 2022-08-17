10d2671066ff1fd272e0fbf5c1f41fdfe2c5272e
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-17 09:54:26 -0300

    aula 156 add: a contact in Whatsapp Clone

--- src/model/User.js
+++ src/model/User.js
@@ -11,20 +11,35 @@ export class User extends Model {
          if(id) this.getById(id);
     }
 
-    get name(){ return this._data.name; }
-    set name(value) {this._data.name = value;}
+    get name(){ 
+        return this._data.name; 
+    }
+
+    set name(value) {
+        this._data.name = value;
+    }
     
-    get email(){ return this._data.email; }
-    set email(value) {this._data.email = value;}
+    get email(){ 
+        return this._data.email; 
+    }
+
+    set email(value) {
+        this._data.email = value;
+    }
     
-    get photo(){ return this._data.photo; }
-    set photo(value) {this._data.photo = value;}
+    get photo(){
+        return this._data.photo; 
+    }
+
+    set photo(value) {
+        this._data.photo = value;
+    }
 
     getById(id){
         return new Promise((s, f )=>{
 
             User.findByEmail(id).onSnapshot(doc=>{
-                
+
                 this.fromJSON(doc.data());
 
                 s(doc);
@@ -43,6 +58,17 @@ export class User extends Model {
 
     static findByEmail(email){
         return User.getRef().doc(email);
+    }
+
+    addContact(contact){
+
+       return User.getRef()
+        .doc(this.email)
+        .collection('contacts')
+        .doc(btoa(contact.email))
+        .set(contact.toJSON());
+
+
     }
 
 }
