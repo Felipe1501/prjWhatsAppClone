27673d5646882cbfb66623a53211236ca34157e9
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-19 09:17:56 -0300

    aula 172 fix: updating user profile data

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -9,7 +9,7 @@ import { Message } from '../model/Message';
 import { Message } from '../model/Message';
 import { Base64 } from "../util/Base64";
 import { ContactsController } from './ContactsController';
-import { Metadata } from 'pdfjs-dist';
+import { Upload } from '../util/Upload';
 
 export class WhatsAppController{
     constructor(){
@@ -386,6 +386,19 @@ export class WhatsAppController{
 
         this.el.photoContainerEditProfile.on('click', e =>{
                 this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputProfilePhoto.on('change', e=>{
+            if (this.el.inputProfilePhoto.files.length > 0){
+                let file = this.el.inputProfilePhoto.files[0];
+
+                Upload.send(file, this._user.email).then(snapshot=>{
+                    this._user.photo = snapshot.downloadURL;
+                    this._user.save().then(()=>{
+                        this.el.btnClosePanelEditProfile.click();
+                    });
+                });
+            }
         });
 
         this.el.inputNamePanelEditProfile.on('keypress', e =>{
